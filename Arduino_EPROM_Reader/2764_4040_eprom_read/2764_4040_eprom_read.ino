8a41ed35a41720c21d0b38ea794adfe72f41578c
Robert <ra.vettel@outlook.com>
2020-05-26 11:50:46 +0200

    eprom data & ino sketches
    These folders contains all the binary EPROM data from my Ursa Major Stargate 323, Serial Number 638, created on May 25th 2020 by me, Robert-André Vettel aka krabbencutter.
    I've used an Arduino Micro & CD4040 & Putty to repeatedly step through all address lines on each IC and log the data. The arduino sketches are included.
    At the point of creation there were already some ROM dumps available at dbwbp.com and I've verified my data against them.
    The data is in binary and each line represents the output of an address line in ascending order.
    "reset" indiciates the point, where the program had reached the highest address available and started again from 0.
    To make sure there are no errors, I have included all data and have only truncated the beginning of each file.
    Everything within the first 256/4096/8192 lines should be correct, but feel free to verify it with different cycles.

---
+++ Arduino_EPROM_Reader/2764_4040_eprom_read/2764_4040_eprom_read.ino
@@ -0,0 +1,81 @@
+int clockPin = 13;
+int resetPin = 12;
+int A12 = 11;
+int q1Pin = 1;
+int q2Pin = 2;
+int q3Pin = 3;
+int q4Pin = 4;
+int q5Pin = 5;
+int q6Pin = 6;
+int q7Pin = 7;
+int q8Pin = 8;
+int q1;
+int q2;
+int q3;
+int q4;
+int q5;
+int q6;
+int q7;
+int q8;
+int i;
+
+void setup() {
+  // put your setup code here, to run once:
+pinMode(clockPin, OUTPUT);
+pinMode(resetPin, OUTPUT);
+pinMode(A12, OUTPUT);
+pinMode(q1Pin, INPUT); 
+pinMode(q2Pin, INPUT); 
+pinMode(q3Pin, INPUT); 
+pinMode(q4Pin, INPUT); 
+}
+
+void loop() {
+digitalWrite(resetPin, 1);
+digitalWrite(resetPin, 0);
+digitalWrite(A12, 0);
+Serial.println("reset");
+for (i=0; i<4096; i++){
+  digitalWrite(clockPin, 1);
+  q1 = digitalRead(q1Pin);
+  q2 = digitalRead(q2Pin);
+  q3 = digitalRead(q3Pin);
+  q4 = digitalRead(q4Pin);
+  q5 = digitalRead(q5Pin);
+  q6 = digitalRead(q6Pin);
+  q7 = digitalRead(q7Pin);
+  q8 = digitalRead(q8Pin);
+  digitalWrite(clockPin, 0);
+  Serial.print(q8);
+  Serial.print(q7);
+  Serial.print(q6);
+  Serial.print(q5);
+  Serial.print(q4);
+  Serial.print(q3);
+  Serial.print(q2);
+  Serial.println(q1);
+}
+digitalWrite(resetPin, 1);
+digitalWrite(resetPin, 0);
+digitalWrite(A12, 1);
+for (i=4096; i<8192; i++){
+  digitalWrite(clockPin, 1);
+  q1 = digitalRead(q1Pin);
+  q2 = digitalRead(q2Pin);
+  q3 = digitalRead(q3Pin);
+  q4 = digitalRead(q4Pin);
+  q5 = digitalRead(q5Pin);
+  q6 = digitalRead(q6Pin);
+  q7 = digitalRead(q7Pin);
+  q8 = digitalRead(q8Pin);
+  digitalWrite(clockPin, 0);
+  Serial.print(q8);
+  Serial.print(q7);
+  Serial.print(q6);
+  Serial.print(q5);
+  Serial.print(q4);
+  Serial.print(q3);
+  Serial.print(q2);
+  Serial.println(q1);
+}
+}
