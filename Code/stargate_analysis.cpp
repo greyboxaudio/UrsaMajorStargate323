830212e255aa8323b309b8a28119ce5f0483a6c6
Robert <ra.vettel@outlook.com>
2024-04-06 09:21:21 +0200

    Update stargate_analysis.cpp

--- Code/stargate_analysis.cpp
+++ Code/stargate_analysis.cpp
@@ -168,7 +168,7 @@ int main()
             }
             if (previousValue != value && (abs(value) >= maxValue || abs(previousValue) >= maxValue))
             {
-                double t1 = 1.0/((j - previousTime + 1) / 32000.0);
+                double t1 = 1.0/((j - previousTime + 1) * 0.00003125);
                 fprintf(fp, "%f Hz %i %f %f\n", t1,j,previousValue,value);
                 previousTime = j;
             }
@@ -187,14 +187,3 @@ int main()
         fclose(fp);
         return 0;
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
