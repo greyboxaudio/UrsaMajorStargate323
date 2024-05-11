9b1487dd82275097772b0b1d5ec217b9c81c33b8
Robert <ra.vettel@outlook.com>
2024-05-11 19:19:57 +0200

    Update 2764_4040_eprom_read.ino

--- Arduino_EPROM_Reader/2764_4040_eprom_read/2764_4040_eprom_read.ino
+++ Arduino_EPROM_Reader/2764_4040_eprom_read/2764_4040_eprom_read.ino
@@ -1,14 +1,14 @@
-int clockPin = 13;
+int clockPin = 11;
 int resetPin = 12;
-int A12 = 11;
-int q1Pin = 1;
-int q2Pin = 2;
-int q3Pin = 3;
-int q4Pin = 4;
-int q5Pin = 5;
-int q6Pin = 6;
-int q7Pin = 7;
-int q8Pin = 8;
+int A12 = 10;
+int q1Pin = 2;
+int q2Pin = 3;
+int q3Pin = 4;
+int q4Pin = 5;
+int q5Pin = 6;
+int q6Pin = 7;
+int q7Pin = 8;
+int q8Pin = 9;
 int q1;
 int q2;
 int q3;
@@ -20,62 +20,72 @@ int q8;
 int i;
 
 void setup() {
-  // put your setup code here, to run once:
-pinMode(clockPin, OUTPUT);
-pinMode(resetPin, OUTPUT);
-pinMode(A12, OUTPUT);
-pinMode(q1Pin, INPUT); 
-pinMode(q2Pin, INPUT); 
-pinMode(q3Pin, INPUT); 
-pinMode(q4Pin, INPUT); 
+  Serial.begin(9600);
+  pinMode(clockPin, OUTPUT);
+  pinMode(resetPin, OUTPUT);
+  pinMode(A12, OUTPUT);
+  pinMode(q1Pin, INPUT_PULLUP);
+  pinMode(q2Pin, INPUT_PULLUP);
+  pinMode(q3Pin, INPUT_PULLUP);
+  pinMode(q4Pin, INPUT_PULLUP);
+  pinMode(q5Pin, INPUT_PULLUP);
+  pinMode(q6Pin, INPUT_PULLUP);
+  pinMode(q7Pin, INPUT_PULLUP);
+  pinMode(q8Pin, INPUT_PULLUP);
 }
 
 void loop() {
-digitalWrite(resetPin, 1);
-digitalWrite(resetPin, 0);
-digitalWrite(A12, 0);
-Serial.println("reset");
-for (i=0; i<4096; i++){
-  digitalWrite(clockPin, 1);
-  q1 = digitalRead(q1Pin);
-  q2 = digitalRead(q2Pin);
-  q3 = digitalRead(q3Pin);
-  q4 = digitalRead(q4Pin);
-  q5 = digitalRead(q5Pin);
-  q6 = digitalRead(q6Pin);
-  q7 = digitalRead(q7Pin);
-  q8 = digitalRead(q8Pin);
-  digitalWrite(clockPin, 0);
-  Serial.print(q8);
-  Serial.print(q7);
-  Serial.print(q6);
-  Serial.print(q5);
-  Serial.print(q4);
-  Serial.print(q3);
-  Serial.print(q2);
-  Serial.println(q1);
-}
-digitalWrite(resetPin, 1);
-digitalWrite(resetPin, 0);
-digitalWrite(A12, 1);
-for (i=4096; i<8192; i++){
-  digitalWrite(clockPin, 1);
-  q1 = digitalRead(q1Pin);
-  q2 = digitalRead(q2Pin);
-  q3 = digitalRead(q3Pin);
-  q4 = digitalRead(q4Pin);
-  q5 = digitalRead(q5Pin);
-  q6 = digitalRead(q6Pin);
-  q7 = digitalRead(q7Pin);
-  q8 = digitalRead(q8Pin);
-  digitalWrite(clockPin, 0);
-  Serial.print(q8);
-  Serial.print(q7);
-  Serial.print(q6);
-  Serial.print(q5);
-  Serial.print(q4);
-  Serial.print(q3);
-  Serial.print(q2);
-  Serial.println(q1);
-}
+  //toggle counter reset
+  digitalWrite(resetPin, 1);
+  digitalWrite(resetPin, 0);
+  //set highest address pin to 0
+  digitalWrite(A12, 0);
+  Serial.println("reset");
+  //step through the first 4096 registers and read the binary data
+  for (i = 0; i < 4096; i++) {
+    digitalWrite(clockPin, 1);
+    q1 = digitalRead(q1Pin);
+    q2 = digitalRead(q2Pin);
+    q3 = digitalRead(q3Pin);
+    q4 = digitalRead(q4Pin);
+    q5 = digitalRead(q5Pin);
+    q6 = digitalRead(q6Pin);
+    q7 = digitalRead(q7Pin);
+    q8 = digitalRead(q8Pin);
+    digitalWrite(clockPin, 0);
+    Serial.print(q8);
+    Serial.print(q7);
+    Serial.print(q6);
+    Serial.print(q5);
+    Serial.print(q4);
+    Serial.print(q3);
+    Serial.print(q2);
+    Serial.println(q1);
+  }
+  //toggle counter reset
+  digitalWrite(resetPin, 1);
+  digitalWrite(resetPin, 0);
+  //set highest address pin to 1
+  digitalWrite(A12, 1);
+  //step through the remaining 4096 registers and read the binary data
+  for (i = 4096; i < 8192; i++) {
+    digitalWrite(clockPin, 1);
+    q1 = digitalRead(q1Pin);
+    q2 = digitalRead(q2Pin);
+    q3 = digitalRead(q3Pin);
+    q4 = digitalRead(q4Pin);
+    q5 = digitalRead(q5Pin);
+    q6 = digitalRead(q6Pin);
+    q7 = digitalRead(q7Pin);
+    q8 = digitalRead(q8Pin);
+    digitalWrite(clockPin, 0);
+    Serial.print(q8);
+    Serial.print(q7);
+    Serial.print(q6);
+    Serial.print(q5);
+    Serial.print(q4);
+    Serial.print(q3);
+    Serial.print(q2);
+    Serial.println(q1);
+  }
 }
